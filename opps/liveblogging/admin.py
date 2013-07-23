2d64b52d008f4563110a34da8742adc9e3c5d9d5
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-07-23 19:10:12 -0300

    Add basic admin class in EventAdmin

---
+++ opps/liveblogging/admin.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
+
+from opps.core.admin import PublishableAdmin
+
+from .models import Event, Message
+
+
+class EventAdmin(PublishableAdmin):
+    raw_id_fields = ['channel', 'main_image']
+    fieldsets = (
+        (_(u'Identification'), {
+            'fields': ('site', 'title', 'slug', 'short_url')}),
+        (_(u'Content'), {
+            'fields': ('hat',
+                       ('main_image', 'main_image_caption'),
+                       'tags')}),
+        (_(u'Relationships'), {
+            'fields': ('channel',)}),
+        (_(u'Publication'), {
+            'classes': ('extrapretty'),
+            'fields': ('published', 'date_available',
+                       'show_on_root_channel',)}),
+    )
+
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(Message)
