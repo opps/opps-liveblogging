3ca76a48e75143f78845290c0b27e009e0198d2d
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-07-23 13:24:48 -0300

    Add setup file on liveblogging

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+
+from opps import liveblogging
+
+
+install_requires = ["opps>=0.2"]
+
+classifiers = ["Development Status :: 4 - Beta",
+               "Intended Audience :: Developers",
+               "Operating System :: OS Independent",
+               "Framework :: Django",
+               'Programming Language :: Python',
+               "Programming Language :: Python :: 2.7",
+               "Operating System :: OS Independent",
+               "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+               'Topic :: Software Development :: Libraries :: Python Modules']
+
+try:
+    long_description = open('README.md').read()
+except:
+    long_description = liveblogging.__description__
+
+setup(name='opps-liveblogging',
+      namespace_packages=['opps'],
+      version=liveblogging.__version__,
+      description=liveblogging.__description__,
+      long_description=long_description,
+      classifiers=classifiers,
+      keywords='Liveblogging app for opps cms, broadcast event via text',
+      author=liveblogging.__author__,
+      author_email=liveblogging.__email__,
+      packages=find_packages(exclude=('doc', 'docs',)),
+      install_requires=install_requires,
+      include_package_data=True,)
