edad6e6928550cc304b1ef34619883ba3a9c4d3e
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-07-24 00:29:05 -0300

    Update author

--- opps/liveblogging/__init__.py
+++ opps/liveblogging/__init__.py
@@ -10,7 +10,7 @@ __version__ = ".".join(map(str, VERSION))
 __status__ = "Development"
 __description__ = u"Liveblogging app, event broadcast via text"
 
-__author__ = u"Thiago AVelino"
+__author__ = u"Thiago Avelino"
 __credits__ = []
 __email__ = u"thiagoavelinoster@gmail.com"
 __copyright__ = u"Copyright 2013, Opps Project"
