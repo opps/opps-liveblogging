5819139fc64f170908873f3fbe0f3fd850cc271d
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-07-24 00:31:12 -0300

    create Message API List, is List Create API

--- opps/liveblogging/views.py
+++ opps/liveblogging/views.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from opps.api.views.generic.list import ListView as ListAPIView
+from opps.api.views.generic.list import ListCreateView
 
-from .models import Event
+from .models import Event, Message
 
 
 class EventAPIList(ListAPIView):
     model = Event
+
+
+class MessageAPIList(ListCreateView):
+    model = Message
