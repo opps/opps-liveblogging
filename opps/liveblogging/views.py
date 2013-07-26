84935b629c6c923cc96fe2ea913594ee4d373dc4
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-07-26 13:39:21 -0300

    Add view event list

--- opps/liveblogging/views.py
+++ opps/liveblogging/views.py
@@ -2,9 +2,13 @@
 # -*- coding: utf-8 -*-
 from opps.api.views.generic.list import ListView as ListAPIView
 from opps.api.views.generic.list import ListCreateView
+from opps.views.generic.list import ListView
 
 from .models import Event, Message
 
+
+class EventList(ListView):
+    model = Event
 
 class EventAPIList(ListAPIView):
     model = Event
