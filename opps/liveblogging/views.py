8d2acda20fe592aea528b59dc4ed542f048b1e2d
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-07-26 19:31:12 -0300

    Add get_context_data super on EventDetail

--- opps/liveblogging/views.py
+++ opps/liveblogging/views.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from django.http import HttpResponse
+
 from opps.api.views.generic.list import ListView as ListAPIView
 from opps.api.views.generic.list import ListCreateView
 from opps.views.generic.list import ListView
 from opps.views.generic.detail import DetailView
 
 from .models import Event, Message
+from .forms import MessageForm
+
+import json
 
 
 class EventList(ListView):
@@ -14,6 +19,16 @@ class EventList(ListView):
 
 class EventDetail(DetailView):
     model = Event
+
+    def get_context_data(self, **kwargs):
+        context = super(EventDetail, self).get_context_data(**kwargs)
+        try:
+            msg = Message.objects.get(event__slug=self.slug)
+        except Message.DoesNotExist:
+            msg = []
+        context['messages'] = msg
+        context['messageform'] = MessageForm
+        return context
 
 
 class EventAPIList(ListAPIView):
