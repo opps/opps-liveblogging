aca199f2f8dc614a4b0d8e8490170e1012cb3fc8
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-07-26 15:23:29 -0300

    Add basic Event Detail view

--- opps/liveblogging/views.py
+++ opps/liveblogging/views.py
@@ -3,12 +3,18 @@
 from opps.api.views.generic.list import ListView as ListAPIView
 from opps.api.views.generic.list import ListCreateView
 from opps.views.generic.list import ListView
+from opps.views.generic.detail import DetailView
 
 from .models import Event, Message
 
 
 class EventList(ListView):
     model = Event
+
+
+class EventDetail(DetailView):
+    model = Event
+
 
 class EventAPIList(ListAPIView):
     model = Event
