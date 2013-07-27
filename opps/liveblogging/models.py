5ea8ac04d3cde80d833ebbefc5debc486ac9a019
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-07-27 18:40:38 -0300

    add method save on Message model, auto set channel

--- opps/liveblogging/models.py
+++ opps/liveblogging/models.py
@@ -5,6 +5,9 @@ from django.utils.translation import ugettext_lazy as _
 
 from opps.core.models import Publishable
 from opps.containers.models import Container
+from opps.channels.models import Channel
+
+from .conf import settings
 
 
 class Event(Container):
@@ -15,3 +18,8 @@ class Message(Publishable):
     event = models.ForeignKey('liveblogging.Event',
                               verbose_name=_(u'Event'))
     message = models.TextField(_(u'Message'))
+
+    def save(self, *args, **kwargs):
+        channel = Channel.objects.get(slug=settings.OPPS_LIVEBLOGGING_CHANNEL)
+        self.channel = channel
+        super(Message, self).save(*args, **kwargs)
