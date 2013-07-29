be5703c0ed56a4ee9dcc7d323f2451cb4034e6b5
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-07-29 17:52:22 -0300

    Add Event Server Detail, use Streaming Http Response in mimetype text event stream

--- opps/liveblogging/views.py
+++ opps/liveblogging/views.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from django.http import HttpResponse
+from django.http import StreamingHttpResponse
+from django.contrib import messages
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 
 from opps.api.views.generic.list import ListView as ListAPIView
 from opps.api.views.generic.list import ListCreateView
@@ -11,6 +15,25 @@ from .models import Event, Message
 from .forms import MessageForm
 
 import json
+import time
+
+
+class EventServerDetail(DetailView):
+    model = Event
+
+    def _longpolling(self, request):
+        while True:
+            yield "data: test streaming server\n\n"
+            time.sleep(0.5)
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        response = StreamingHttpResponse(self._longpolling(request),
+                                         mimetype='text/event-stream')
+        response['Cache-Control'] = 'no-cache'
+        response['Software'] = 'opps-liveblogging'
+        response.flush()
+        return response
 
 
 class EventAdmin(object):
@@ -41,8 +64,11 @@ class EventAdminDetail(EventAdmin, DetailView):
         msg = request.POST['message']
         obj = Message.objects.create(message=msg, user=request.user,
                                      event=self.get_object(), published=True)
-        resp = {'menssage': msg, 'status': obj.published}
-        return HttpResponse(json.dumps(resp), mimetype="application/json")
+        resp = json.dumps({'menssage': msg, 'status': obj.published})
+
+        # Generate message event INFO
+        messages.add_message(request, messages.INFO, resp)
+        return HttpResponse(resp, mimetype="application/json")
 
 
 class EventAPIList(ListAPIView):
