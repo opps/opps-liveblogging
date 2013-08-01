95ace95cb5935bddbd1b7b18395fd802c27da5a1
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-08-01 20:38:25 -0300

    Set dynamic kwargs channel longslug on urls

--- opps/liveblogging/urls.py
+++ opps/liveblogging/urls.py
@@ -13,10 +13,10 @@ urlpatterns = patterns(
     '',
     url(r'^admin/liveblogging/message/(?P<slug>[\w-]+)$',
         login_required(EventAdminDetail.as_view()), name='event-admin-detail',
-        kwargs={'channel__long_slug': u'live'}),
+        kwargs={'channel__long_slug': settings.OPPS_LIVEBLOGGING_CHANNEL}),
     url(r'^admin/liveblogging/message/$',
         login_required(EventAdminList.as_view()), name='event-admin-list',
-        kwargs={'channel__long_slug': u'live'}),
+        kwargs={'channel__long_slug': settings.OPPS_LIVEBLOGGING_CHANNEL}),
 )
 
 urlpatterns += patterns(
@@ -24,21 +24,21 @@ urlpatterns += patterns(
     url(r'^{}/(?P<slug>[\w-]+)$'.format(
         settings.OPPS_LIVEBLOGGING_CHANNEL),
         EventDetail.as_view(), name='event',
-        kwargs={'channel__long_slug': u'live'}),
+        kwargs={'channel__long_slug': settings.OPPS_LIVEBLOGGING_CHANNEL}),
 
     url(r'^{}/server/(?P<slug>[\w-]+)$'.format(
         settings.OPPS_LIVEBLOGGING_CHANNEL),
         EventServerDetail.as_view(), name='event-server',
-        kwargs={'channel__long_slug': u'live'}),
+        kwargs={'channel__long_slug': settings.OPPS_LIVEBLOGGING_CHANNEL}),
 
     url(r'^{}/event/(?P<event__id>\d+).api$'.format(
         settings.OPPS_LIVEBLOGGING_CHANNEL),
         cache_page(settings.OPPS_CACHE_EXPIRE_DETAIL)(
             MessageAPIList.as_view()), name='event-api',
-        kwargs={'channel__long_slug': u'live'}),
+        kwargs={'channel__long_slug': settings.OPPS_LIVEBLOGGING_CHANNEL}),
 
     url(r'^{}/events.api$'.format(settings.OPPS_LIVEBLOGGING_CHANNEL),
         cache_page(settings.OPPS_CACHE_EXPIRE_DETAIL)(
             EventAPIList.as_view()), name='events-api',
-        kwargs={'channel__long_slug': u'live'}),
+        kwargs={'channel__long_slug': settings.OPPS_LIVEBLOGGING_CHANNEL}),
 )
