9e5b95ca00e22524596328c6a61778b67432b38f
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-11-06 13:31:48 -0200

    Remove old api urls

--- opps/liveblogging/urls.py
+++ opps/liveblogging/urls.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, url
-from django.views.decorators.cache import cache_page
 from django.contrib.auth.decorators import login_required
 
-from .views import EventAPIList, MessageAPIList, EventServerDetail
+from .views import EventServerDetail
 from .views import EventAdminList, EventAdminDetail, EventDetail
 from .conf import settings
 
@@ -29,16 +28,5 @@ urlpatterns += patterns(
     url(r'^{}/server/(?P<slug>[\w-]+)$'.format(
         settings.OPPS_LIVEBLOGGING_CHANNEL),
         EventServerDetail.as_view(), name='event-server',
-        kwargs={'channel__long_slug': settings.OPPS_LIVEBLOGGING_CHANNEL}),
-
-    url(r'^{}/event/(?P<event__id>\d+).api$'.format(
-        settings.OPPS_LIVEBLOGGING_CHANNEL),
-        cache_page(settings.OPPS_CACHE_EXPIRE_DETAIL)(
-            MessageAPIList.as_view()), name='event-api',
-        kwargs={'channel__long_slug': settings.OPPS_LIVEBLOGGING_CHANNEL}),
-
-    url(r'^{}/events.api$'.format(settings.OPPS_LIVEBLOGGING_CHANNEL),
-        cache_page(settings.OPPS_CACHE_EXPIRE_DETAIL)(
-            EventAPIList.as_view()), name='events-api',
         kwargs={'channel__long_slug': settings.OPPS_LIVEBLOGGING_CHANNEL}),
 )
