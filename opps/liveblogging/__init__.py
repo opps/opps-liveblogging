3b6fb6433a7dd09940fd145b53a5bc3f7d74f558
Thiago Avelino <thiago@avelino.xxx>
2014-06-21 12:07:25 -0300

    upgrade license year

--- opps/liveblogging/__init__.py
+++ opps/liveblogging/__init__.py
@@ -4,7 +4,7 @@ import pkg_resources
 
 pkg_resources.declare_namespace(__name__)
 
-VERSION = (0, 1, 0)
+VERSION = (0, 1, 1)
 
 __version__ = ".".join(map(str, VERSION))
 __status__ = "Development"
@@ -12,5 +12,5 @@ __description__ = u"Liveblogging app, event broadcast via text"
 
 __author__ = u"Thiago Avelino"
 __credits__ = []
-__email__ = u"thiagoavelinoster@gmail.com"
-__copyright__ = u"Copyright 2013, Opps Project"
+__email__ = u"thiago@avelino.xxx"
+__copyright__ = u"Copyright 2014, Thiago Avelino"
